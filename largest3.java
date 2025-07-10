f4a5457e67afcc0ccf602b821d89893c3159fbb6
Onkar Shinde <onkarshinde499@gmail.com>
2025-07-10 12:34:51 +0530

    largest of 3

---
+++ largest3.java
@@ -0,0 +1,15 @@
+public class largest3 {
+    public static void main(String args[]) {
+        int a = 1000, b = 70, c = 300;
+
+        if ((a > b) && (a > c)) {
+            System.out.println("A is a largest number.");
+        }else if((b > a) && (b > c)){
+            System.out.println("B is a largst number");
+        }else{
+            System.out.println("C is a largest number");
+        }
+
+    }
+
+}
