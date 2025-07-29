af418c990f5b60159c8c5b901989c68bf4b90cbc
Onkar Shinde <onkarshinde499@gmail.com>
2025-07-29 08:52:00 +0530

    SUB ARRAYS OF ARR AND THEIR SUM MIN AND MAZ SUM

--- subarrays.java
+++ subarrays.java
@@ -1,29 +1,42 @@
 public class subarrays {
     public static void pairs(int arr[]) {
         int tp = 0;
+        int maxSum = Integer.MIN_VALUE;  // Initially the smallest possible value
+        int minSum = Integer.MAX_VALUE;  // Initially the largest possible value
+
         for (int i = 0; i < arr.length; i++) {
             int start = i;
             for (int j = i; j < arr.length; j++) {
                 int end = j;
                 int sum = 0;
+
                 for (int k = start; k <= end; k++) {
-                    System.out.print(arr[k] + " ," );
+                    System.out.print(arr[k] + " ,");
                     sum = sum + arr[k];
-                    tp++;
                 }
 
-                System.out.print("    => sum of array is  = " +sum);
+                // Update max and min sums
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+                if (sum < minSum) {
+                    minSum = sum;
+                }
+
+                System.out.print("    => sum of array is = " + sum);
+                tp++;
                 System.out.println();
             }
             System.out.println();
         }
-        System.out.println("total pairs are " + tp);
+
+        System.out.println("Total pairs (subarrays): " + tp);
+        System.out.println("Maximum subarray sum: " + maxSum);
+        System.out.println("Minimum subarray sum: " + minSum);
     }
 
     public static void main(String args[]) {
         int arr[] = { 11, 22, 33, 44, 55, 66, 77, 88, 99 };
-
         pairs(arr);
-
     }
 }
