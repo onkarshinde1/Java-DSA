85ae921f1f837ec3104d63ecf0b8a571f07bdbe9
Onkar Shinde <onkarshinde499@gmail.com>
2025-07-29 08:37:00 +0530

    SUB ARRAYS OF ARR

---
+++ subarrays.java
@@ -0,0 +1,26 @@
+public class subarrays {
+    public static void pairs(int arr[]) {
+        int tp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ," );
+                    tp++;
+                }
+
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("total pairs are " + tp);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+        pairs(arr);
+
+    }
+}
