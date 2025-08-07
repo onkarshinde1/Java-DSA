c71c58c171f07bf9a9bf0f9e2d16a2000af232ba
Onkar Shinde <onkarshinde499@gmail.com>
2025-08-07 09:23:31 +0530

    Insertion Sort:

--- Bubblesort.java
+++ Bubblesort.java
@@ -2,24 +2,27 @@ public class Bubblesort {
 
     public static void Selection(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 -i; j++) {
+            int swap =0;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap++;
                 }
             }
+            System.out.println(swap);
         }
     }
 
-    public static void printArr(int arr[]){
-        for(int i = 0; i< arr.length;i++){
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2,7,8,6,9 };
+        int arr[] = {1,2,3,4,5};
         Selection(arr);
         printArr(arr);
     }
