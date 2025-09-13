e08a357eba31b864793135b58de3c951e341d2eb
Onkar Shinde <onkarshinde499@gmail.com>
2025-09-13 08:58:01 +0530

    counting sort

---
+++ countingSort.java
@@ -0,0 +1,37 @@
+public class countingSort {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorthing
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 2, 3, 4, 2, 7, 8, 4 };
+        countingSort(arr);
+        printArr(arr);
+    }
+}
