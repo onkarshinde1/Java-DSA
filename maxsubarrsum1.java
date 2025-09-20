c292ec0ecdee45debf17689f7abe24740b90d84a
Onkar Shinde <onkarshinde499@gmail.com>
2025-09-20 07:58:17 +0530

    max sub arr sum problems

--- maxsubarrsum1.java
+++ maxsubarrsum1.java
@@ -25,7 +25,7 @@ public class maxsubarrsum1 {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, -2, 6, -1, 3 ,2 ,4};
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
         pairs(arr);
     }
 }
