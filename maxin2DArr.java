e7faf0bfcbc306c239638f7b0c521e9907e5563f
Onkar Shinde <onkarshinde499@gmail.com>
2025-12-15 11:23:55 +0530

    max elem in 2D Arr

---
+++ maxin2DArr.java
@@ -0,0 +1,28 @@
+public class maxin2DArr {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3, 4 },
+                { 12, 14, 15, 51 },
+                { 11, 16, 17, 18 },
+                { 6, 7, 9 },
+                { 555 },
+                { 45, 89, 99 }
+        };
+
+        System.out.println(max(arr));
+
+    }
+
+    public static int max(int[][] arr){
+        int maximum = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for(int col = 0 ; col < arr[row].length;col++){
+                if(arr[row][col] > maximum){
+                    maximum = arr[row][col];
+                
+                }
+            }
+        }
+        return maximum;
+    }
+}
