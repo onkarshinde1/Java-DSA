6eb99dcea35fcb045fda36b39495b7d2c7b711e7
Onkar Shinde <onkarshinde499@gmail.com>
2025-12-25 07:06:14 +0530

    Celling number in array

---
+++ cellingNum.java
@@ -0,0 +1,30 @@
+public class cellingNum {
+    public static void main(String[] args) {
+        int[] arr = {-20, -15, -8, -3, 0, 4, 9, 15, 22, 30 };
+        int ans = celling(arr, 45);
+        System.out.println(ans);
+    }
+
+    public static int celling(int[] arr ,int target){
+        
+        int start = 0;
+        int end = arr.length-1;
+        //if the target element is greater than the gretest number in array it should return the -1
+        // boundry condition
+        if(target > end){
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) /2;
+
+            if(target > arr[mid]){
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+
+        }
+        return start;
+    }
+}
